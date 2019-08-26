77621fbc10f582ce05e9829e7dfe2c0c02a06688
andregodasi <andre.goncalves@wises.com.br>
2019-08-26 18:33:54 -0300

    implementação do header

---
+++ src/app/shared/components/pokeball/pokeball.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokeballComponent } from './pokeball.component';
+
+
+
+@NgModule({
+  declarations: [PokeballComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokeballModule { }
